b49e812cc8d5abd0d9473c1cca141e2e6e566474
Milansindhav22 <milanmax00700@gmail.com>
2026-01-08 00:25:07 +0530

    Update CSV file path for student performance data

--- projects/student_performance analysis/student performance.py
+++ projects/student_performance analysis/student performance.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv("student_performance.csv")
+df = pd.read_csv("data/student_performance.csv")
 
 pass_fail = df['Result'].value_counts()
 
